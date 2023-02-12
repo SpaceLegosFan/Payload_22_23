4adbc5ad59b362dd8eb26a22b01afaf5b5874cb8
SpaceLegosFan <115024167+SpaceLegosFan@users.noreply.github.com>
2023-02-12 15:07:03 -0500

    added serial input and direction

--- Main Code/Test_Code/StepperMotorTesting.ino
+++ Main Code/Test_Code/StepperMotorTesting.ino
@@ -1,4 +1,4 @@
-// NDRT Stepper Motor Testing
+// NDRT Deployment Testing
 // Link to example: https://microcontrollerslab.com/stepper-motor-a4988-driver-module-esp32/
 
 #include <AccelStepper.h>
@@ -16,11 +16,7 @@
 //A1 BLUE
 //B1 RED
 
-// Stepper Motor starts in the up direction! Major sure actuation is moving up!
-
-
-
-
+int movementDirection; // 1 for moving up |||| -1 for moving down
 const int DIR = 2;
 const int STEP = 3;
 
@@ -29,7 +25,7 @@ AccelStepper LeadScrewStepper(motorInterfaceType, STEP, DIR);
 //AccelStepper CameraStepper(motorInterfaceType, STEP, DIR);
 
 // System Properties
-float travel_distance = 9.6;//8.63; // ask spencer or https://drive.google.com/drive/u/0/folders/1Yd59MVs0kGjNgtfuYpVg5CDFZwnHGlRj
+float travel_distance = 8.63; //9.6;//8.63; // ask spencer or https://drive.google.com/drive/u/0/folders/1Yd59MVs0kGjNgtfuYpVg5CDFZwnHGlRj
 
 // Lead Screw Properties 
 // Part link https://www.mcmaster.com/8677N21/
@@ -37,20 +33,27 @@ float num_steps = 400; // steps per rotation; this would be if we are half-stepp
 float travel_distance_per_full_step = 0.00125; // inches/step
 
 //Motion Calculations
-float num_deployment_LeadScrew_steps = travel_distance / travel_distance_per_full_step;
+float num_deployment_LeadScrew_steps = movementDirection*travel_distance / travel_distance_per_full_step;
 
 void setup() {
   pinMode(STEP, OUTPUT);
   pinMode(DIR, OUTPUT);
   Serial.begin(115200);
 
-  delay(1000);
-  
+  Serial.begin(115200);
+  Serial.println("Enter 1 to deploy lead screw cover or -1 to retract lead screw cover");
+while (Serial.available() == 0) {
+;
+}
+movementDirection = Serial.parseInt();
+Serial.println(movementDirection);
+delay(1000);
+
   // Lead Screw Stepper (Primary)
-  LeadScrewStepper.setMaxSpeed(800);
+  LeadScrewStepper.setMaxSpeed(400); //800
   LeadScrewStepper.setAcceleration(1000);
   LeadScrewStepper.setSpeed(500);
-  LeadScrewStepper.moveTo(-num_deployment_LeadScrew_steps);
+  LeadScrewStepper.moveTo(num_deployment_LeadScrew_steps);
   //LeadScrewStepper.runSpeedToPosition(); // Blocks until all are in position
 }
 
