78689d385268026dc38e9c0e51bd900c575bd084
RYANPND <123332740+RYANPND@users.noreply.github.com>
2023-02-18 23:08:02 -0500

    Create WifiSerial.ino
    Web, but with serial.

---
+++ Main Code/Test_Code/WifiSerial.ino
@@ -0,0 +1,52 @@
+// ** DO NOT BUY THE WebSerialPro library**. I have found it and modified it, if you wish to upload this code, I will give you the library directly.
+// ** If you do not have access to the WebSerialPro library, simply change it to WebSerial library instead. Again, come to me if you want the WebSerialpro library.
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSerialPro.h>
+
+#define LED 2
+
+AsyncWebServer server(80);
+
+const char* ssid = "ND-guest";          // Your WiFi SSID
+const char* password = "";  // Your WiFi Password
+
+void recvMsg(uint8_t *data, size_t len){
+  WebSerialPro.println("Received Data...");
+  String d = "";
+  for(int i=0; i < len; i++){
+    d += char(data[i]);
+  }
+  WebSerialPro.println(d);
+  if (d == "ON"){
+    digitalWrite(LED, HIGH);
+  }
+  if (d=="OFF"){
+    digitalWrite(LED, LOW);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("WiFi Failed!\n");
+    return;
+  }
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  // WebSerial is accessible at "<IP Address>/webserial" in browser
+  WebSerialPro.begin(&server);
+  WebSerialPro.msgCallback(recvMsg);
+  server.begin();
+}
+
+void loop() {
+  WebSerialPro.println("Hello!");
+  delay(2000);
+}
