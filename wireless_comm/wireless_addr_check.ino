a78a3de940440f5f2c9e55c22215096c9cea74ab
jelwellndrt <119386213+jelwellndrt@users.noreply.github.com>
2023-02-19 19:09:57 -0500

    Add files via upload

---
+++ wireless_comm/wireless_addr_check.ino
@@ -0,0 +1,18 @@
+/*****
+
+  Jacob Elwell
+  Finds ESP32 MAC address.
+
+*****/
+
+#include "WiFi.h"
+ 
+void setup(){
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_STA);
+  Serial.println(WiFi.macAddress());
+}
+ 
+void loop(){
+
+}
