7a6b2ba83bd88c1e1be4a2e1402b76457be92433
huntertkoch12 <117240053+huntertkoch12@users.noreply.github.com>
2023-02-21 19:39:10 -0500

    Update TROI-Main.ino

--- Main Code/TROI-Main.ino
+++ Main Code/TROI-Main.ino
@@ -66,7 +66,7 @@ const char* ssid_backup = "ND-guest";
 const char* password_backup = "";
 
 // ESP-NOW - THIS NEEDS TO BE CHANGED, MAC ADDRESS NOT VALID
-uint8_t broadcastAddress[] = {0xC8, 0xF0, 0x9E, 0x9A, 0x83, 0x68};
+uint8_t broadcastAddress[] = {0xC8, 0xF0, 0x9E, 0x9D, 0x46, 0x40};
 typedef struct struct_message {
   char timestamp[32];
   int command;
