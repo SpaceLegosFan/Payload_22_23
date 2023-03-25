7d45a3b5af32e23ba7a2d1f3a28aa30596fb29b4
huntertkoch12 <117240053+huntertkoch12@users.noreply.github.com>
2023-03-25 15:10:10 -0400

    Update TROI-Main (PCB) Code.ino

--- Main Code/TROI-Main (PCB) Code.ino
+++ Main Code/TROI-Main (PCB) Code.ino
@@ -28,97 +28,9 @@ TROI ESP32-Main Code
 #define GYRO_LAND_TOLERANCE 5
 #define ACCELERATION_LAUNCH_TOLERANCE 30
 #define DEPLOYSTEPS 9400
-
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
-#define REST      0
+#define REST 0
+#define BUZZER 4
+#define TEMPO 108
 
 TwoWire I2CSensors = TwoWire(0);
 TwoWire I2CSensors2 = TwoWire(1);
@@ -135,41 +47,29 @@ int size = 0;
 // Data Storage
 int address = 0;
 
-// Music
-
-int playing = 0;
-
-int tempo = 108;
-int buzzer = 11;
-
 int melody[] = {
+  466,8, 466,8, 466,8, //1
+  698,2, 1047,2,
+  932,8, 880,8, 784,8, 698,2, 1047,4,
+  932,8, 880,8, 784,8, 698,2, 1047,4,
+  932,8, 880,8, 932,8, 784,2, 523,8, 523,8, 523,8,
+  698,2, 1047,2,
+  932,8, 880,8, 784,8, 698,2, 1047,4,
 
-  NOTE_AS4,8, NOTE_AS4,8, NOTE_AS4,8,//1
-  NOTE_F5,2, NOTE_C6,2,
-  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4,  
-  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4,  
-  NOTE_AS5,8, NOTE_A5,8, NOTE_AS5,8, NOTE_G5,2, NOTE_C5,8, NOTE_C5,8, NOTE_C5,8,
-  NOTE_F5,2, NOTE_C6,2,
-  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4,  
-  
-  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4, //8  
-  NOTE_AS5,8, NOTE_A5,8, NOTE_AS5,8, NOTE_G5,2, NOTE_C5,-8, NOTE_C5,16, 
-  NOTE_D5,-4, NOTE_D5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
-  NOTE_F5,8, NOTE_G5,8, NOTE_A5,8, NOTE_G5,4, NOTE_D5,8, NOTE_E5,4,NOTE_C5,-8, NOTE_C5,16,
-  NOTE_D5,-4, NOTE_D5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
-  
-  NOTE_C6,-8, NOTE_G5,16, NOTE_G5,2, REST,8, NOTE_C5,8,//13
-  NOTE_D5,-4, NOTE_D5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
-  NOTE_F5,8, NOTE_G5,8, NOTE_A5,8, NOTE_G5,4, NOTE_D5,8, NOTE_E5,4,NOTE_C6,-8, NOTE_C6,16,
-  NOTE_F6,4, NOTE_DS6,8, NOTE_CS6,4, NOTE_C6,8, NOTE_AS5,4, NOTE_GS5,8, NOTE_G5,4, NOTE_F5,8,
-  NOTE_C6,1
-  
+  932,8, 880,8, 784,8, 698,2, 1047,4, //8
+  932,8, 880,8, 932,8, 784,2, 523,-8, 523,16,
+  587,-4, 587,8, 932,8, 880,8, 784,8, 698,8,
+  698,8, 784,8, 880,8, 784,4, 587,8, 659,4,523,-8, 523,16,
+  587,-4, 587,8, 932,8, 880,8, 784,8, 698,8,
+
+  1047,-8, 784,16, 784,2, 0,8, 523,8,//13
+  587,-4, 587,8, 932,8, 880,8, 784,8, 698,8,
+  698,8, 784,8, 880,8, 784,4, 587,8, 659,4,1047,-8, 1047,16,
+  1397,4, 622,8, 554,4, 523,8, 466,4, 415,8, 392,4, 349,8,
+  1047,1
 };
-
 int notes = sizeof(melody) / sizeof(melody[0]) / 2;
-
-int wholenote = (60000 * 4) / tempo;
-
+int wholenote = (60000 * 4) / TEMPO;
 int divider = 0, noteDuration = 0;
 
 // Motor Values
@@ -270,6 +170,7 @@ void setup() {
     }
   }
   printEvent("We Have Launched!");
+  playNote(440, 20000, 1);
 
   //Launched State, 0 = Failed, 1 = Success
   writeTrue();
@@ -280,6 +181,7 @@ void setup() {
     if (i % 100 == 0){
       char timeMessage[50];
       snprintf(timeMessage, 50, "We are %d seconds into flight!", i/10);
+      playNote(440, 200, 1);
       printEvent(timeMessage);
     }
     updateLanding();
@@ -288,6 +190,7 @@ void setup() {
 
   // Wait in standby mode and loop until landed
   printEvent("Standing By for Landing");
+  playNote(440, 200, 3);
 
   //Seconds Passed, Waiting for Landing , 0 = Failed, 1 = Success
   writeTrue();
@@ -343,6 +246,9 @@ void setup() {
   writeTrue();
 
   printEvent("Standing By for Camera commands...");
+  playNote(440, 200, 1);
+  delay(500);
+  playNote(440, 200, 2);
 	serialMessage = "";
 }
 
@@ -734,28 +640,24 @@ void writeMillis() {
 }
 
 void playMusic() {
-
-for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
-
-    // calculates the duration of each note
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
     divider = melody[thisNote + 1];
     if (divider > 0) {
-      // regular note, just proceed
       noteDuration = (wholenote) / divider;
     } else if (divider < 0) {
-      // dotted notes are represented with negative durations!!
       noteDuration = (wholenote) / abs(divider);
       noteDuration *= 1.5; // increases the duration in half for dotted notes
     }
-
-    // we only play the note for 90% of the duration, leaving 10% as a pause
-    tone(buzzer, melody[thisNote], noteDuration*0.9);
-
-    // Wait for the specief duration before playing the next note.
+    tone(BUZZER, melody[thisNote], noteDuration*0.9);
     delay(noteDuration);
-    
-    // stop the waveform generation before the next note.
-    noTone(buzzer);
+    noTone(BUZZER);
   }
+}
 
+void playNote(int note, int length, int numTimes) {
+  for(int i = 0; i < numTimes; i++) {
+    tone(BUZZER, note, length);
+    delay(length);
+    noTone(BUZZER);
+  }
 }
